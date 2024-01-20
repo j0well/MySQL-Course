8dda12e218e88fb141f93c4a9d078a45f5b6af2b
j0well <126909236+j0well@users.noreply.github.com>
2024-01-20 13:14:41 -0500

    Lesson 12
    Lesson Objectives:
    * Perform database backups and restores.
    * Export data from a database to a file.
    * Import data from a file to a database.
    * Understand different backup strategies such as full backup and incremental backup.
    * Work with tab-delimited and comma-delimited files for data import/export.
    * Check and repair InnoDB or MyISAM tables for possible corruption

---
+++ Lesson 12/CIS276DA_Lesson12Exercise4_jow2148782.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: select * from vendors
+where default_account_number = '777'
+LIMIT 0, 1000
+
+-- Date: 2024-01-05 16:22
+*/
+INSERT INTO `` (`vendor_id`,`vendor_name`,`vendor_address1`,`vendor_address2`,`vendor_city`,`vendor_state`,`vendor_zip_code`,`vendor_phone`,`vendor_contact_last_name`,`vendor_contact_first_name`,`default_terms_id`,`default_account_number`) VALUES (125,'Juicing','123 Wolly Way',NULL,'Slidell','LA','92111','6195985263','Jones','Allen',1,777);
